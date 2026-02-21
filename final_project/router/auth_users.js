4cd832c7071fb728622f50274b145836652e163e
Raj Chaudhari <rajchaudharii1403@gmail.com>
2026-02-21 23:14:50 +0530

    Add route to delete user reviews by ISBN

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -65,6 +65,27 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   });
 });
 
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+
+  const isbn = req.params.isbn;
+  const username = req.body.username;   // passing via curl body
+
+  if (!books[isbn]) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  if (!books[isbn].reviews[username]) {
+    return res.status(404).json({ message: "Review not found for this user" });
+  }
+
+  delete books[isbn].reviews[username];
+
+  return res.status(200).json({
+    message: "Review successfully deleted",
+    reviews: books[isbn].reviews
+  });
+});
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
