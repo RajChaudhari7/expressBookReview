cd23c8a52f286385389618b03583b46cd8acc418
Raj Chaudhari <rajchaudharii1403@gmail.com>
2026-02-21 23:11:45 +0530

    Add review functionality for books
    Implemented functionality to add or modify book reviews.

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -47,10 +47,22 @@ regd_users.post("/login", (req,res) => {
   }
 });
 
-// Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const isbn = req.params.isbn;
+  const review = req.body.review;
+  const username = req.body.username;   // since using curl directly
+
+  if (!books[isbn]) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  books[isbn].reviews[username] = review;
+
+  return res.status(200).json({
+    message: "Review successfully added/modified",
+    reviews: books[isbn].reviews
+  });
 });
 
 module.exports.authenticated = regd_users;
